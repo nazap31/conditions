5e67df5322cbb1ae40d3854afada743ebe8fb2a6
nazap31 <you@example.com>
2022-06-20 23:21:31 -0600

    conditions

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("C# condiciones");
+
+Console.WriteLine("Porfavor ingrese la nota del estudiante");
+double grade = Double.Parse (Console.ReadLine() ?? "0");
+Console.WriteLine("El estudiante pago (y/n)");
+//bool paid = (Console.ReadLine() == "y");
+bool paid;
+string input = Console.ReadLine() ?? "n";
+if (input == "y")
+{
+    paid = true;
+} else {
+    paid = false;
+}
+
+if (grade >= 70)
+{
+    if (paid)
+    {
+        Console.WriteLine("Felicidades!");
+    } else {
+        Console.WriteLine("Necesitas pagar la graduacion");
+    }
+} else {
+    Console.WriteLine("No esta graduado");
+}
+
+Console.WriteLine("Programa terminado!");
